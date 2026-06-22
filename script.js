759afb8269906aa0596294ef057c4088254b7dc9
AungThatZin <aungthzin2007@gmail.com>
2026-06-22 10:41:16 +0630

    customize menu bar

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ const menuHtml = `
             <a href="DefaultPlusUserPrompt.html" class="menu-link">📝 Default Prompt</a>
             <a href="BaseballBatAIPrompt.html" class="menu-link">⚾ Baseball Bat AI Prompt</a>
             <a href="Aifashion.html" class="menu-link">🎨 AI Fashion</a>
+                        <a href="Tshirt.html" class="menu-link">Tshirt</a>
+
         </div>
     </nav>
 `;
